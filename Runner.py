2a81c8e160ac36bd6f832d7f63dd96f58d8c57bc
nilmit <akegb10@yahoo.se>
2026-06-06 23:03:12 +0200

    removed old code, parameter settings

--- Runner.py
+++ Runner.py
@@ -10,49 +10,6 @@ class Reader:
     def get_dataframe(self, file: str) -> pd.DataFrame:
         raise NotImplementedError
 
-class ReaderFootballDataCoUK(Reader):
-    def __init__(self):
-        self.headers_to_remove = [
-            "Country",
-            "League",
-            "Season",
-            "Time",
-        ]
-
-    def _get_raw_dataframe(self, file: str) -> pd.DataFrame:
-        return pd.read_csv(file, sep=",")
-
-    def _clean_and_order_data(self, df: pd.DataFrame) -> pd.DataFrame:
-        df = df.sort_values("Date", ascending=True)
-        df = df.drop(columns=self.headers_to_remove)
-        print(df)
-        return df
-
-    def _keep_only_best_bets(self, df:pd.DataFrame)->pd.DataFrame:
-        home_cols = [col for col in df.columns if "CH" in col]
-        tie_cols = [col for col in df.columns if "CD" in col]
-        away_cols = [col for col in df.columns if "CA" in col]
-        best_bets = pd.DataFrame({
-            "home_decimal": df[home_cols].max(axis=1),
-            "tie_decimal": df[tie_cols].max(axis=1),
-            "away_decimal": df[away_cols].max(axis=1)
-        })
-        df = df.drop(columns=home_cols + tie_cols + away_cols)
-        return pd.concat([df, best_bets], axis=1)
-
-    # ett gemensamt format som alla ska ha - detta måste fixas
-    def _convert_to_format(self, df: pd.DataFrame) -> pd.DataFrame:
-        return df
-
-    def get_dataframe(self, file) -> pd.DataFrame:
-        df = self._get_raw_dataframe(file)
-        df = self._clean_and_order_data(df)
-        df = self._keep_only_best_bets(df)
-        df = self._convert_to_format(df)
-        return df
-
-
-# "gameID","leagueID","season","date","homeTeamID","awayTeamID","homeGoals","awayGoals","homeProbability","drawProbability","awayProbability","homeGoalsHalfTime","awayGoalsHalfTime","B365H","B365D","B365A","BWH","BWD","BWA","IWH","IWD","IWA","PSH","PSD","PSA","WHH","WHD","WHA","VCH","VCD","VCA","PSCH","PSCD","PSCA"
 
 class ReaderGeneral(Reader):
     def __init__(self, headers_to_remove: list[str], home_bet_re:str, tie_bet_re:str, away_bet_re:str, date:str, home:str, away:str, hg:str, ag:str, date_format:str):
@@ -87,16 +44,11 @@ class ReaderGeneral(Reader):
         df = df.drop(columns=home_cols + tie_cols + away_cols)
         return pd.concat([df, best_bets], axis=1)
 
-    # ett gemensamt format som alla ska ha - detta måste fixas
-    # "gameID","leagueID","season","date","homeTeamID","awayTeamID","homeGoals","awayGoals","homeProbability","drawProbability","awayProbability","homeGoalsHalfTime","awayGoalsHalfTime","B365H","B365D","B365A","BWH","BWD","BWA","IWH","IWD","IWA","PSH","PSD","PSA","WHH","WHD","WHA","VCH","VCD","VCA","PSCH","PSCD","PSCA"
-
-    # Country,League,Season,Date,Time,Home,Away,HG,AG,Res,PSCH,PSCD,PSCA,MaxCH,MaxCD,MaxCA,AvgCH,AvgCD,AvgCA,BFECH,BFECD,BFECA,B365CH,B365CD,B365CA
     def _convert_to_format(self, df: pd.DataFrame) -> pd.DataFrame:
         df = df.rename(columns={self.home: "Home", self.away: "Away", self.hg: "HG", self.ag: "AG", self.date: "Date"})
         df = df.astype({"Home": str, "Away": str, "HG": int, "AG": int}) 
         df["Date"] = pd.to_datetime(df["Date"], format=self.date_format)
         return df
-
 
     def get_dataframe(self, file) -> pd.DataFrame:
         df = self._get_raw_dataframe(file)
@@ -127,30 +79,42 @@ class StateInitializer:
 # sloppy test code
 def test():
     initial_team_dist = Model.NormalDistribution(Model.DEFAULT_START_MU, Model.DEFAULT_START_VAR)
-    initial_home_advantage_dist = Model.NormalDistribution(0, 10)
+    initial_home_advantage_dist = Model.NormalDistribution(0.16, 10)
     default_dist = Model.NormalDistribution(Model.DEFAULT_START_MU, Model.DEFAULT_START_VAR)
 
+    # Choose datafile here
     reader = ReaderGeneral(["leagueID", "season"], r"C?H$", r"^(?!.*ID$).*C?D$", r"C?A$", "date", "homeTeamID", "awayTeamID", "homeGoals", "awayGoals", "%Y-%m-%d %H:%M:%S")
     datafile = "./datasets/games.csv" # kaggle
 
     # reader = ReaderGeneral(["League", "Season"], r"C?H$", r"^(?!.*ID$).*C?D$", r"C?A$", "Date", "Home", "Away", "HG", "AG", "%d/%m/%Y")
     # datafile = "./datasets/SWE.csv" #  footballfatacouk
 
+    # Choose skill model
+
     df, state = StateInitializer(reader, Model.AttackDefenseSkill(default_dist, default_dist)).initialize_modelstate(datafile)
+    model = Model.TrueskillAdvancedAttackDefense(Model.DEFAULT_VAR_T, Model.DEFAULT_VAR_T)
+    adapter = Model.TrueskillAdvancedAttackDefenseAdapter(model)
+
+    # df, state = StateInitializer(reader, Model.GeneralSkill(default_dist)).initialize_modelstate(datafile)
+    # model = Model.TrueskillAdvanced(Model.DEFAULT_VAR_T, 1)
+    # adapter = Model.TrueskillAdvancedAdapter(model)
+    # model = Model.TrueSkillBasicGibbsSampling(Model.TrueSkillModelBasic(Model.DEFAULT_VAR_T), 200, 2000)
+    # adapter = Model.TrueskillBasicAdapter(model)
+    # model = Model.TrueSkillBasicMessagePassing()
+    # adapter = Model.TrueskillBasicAdapter(model)
+
+    min_observed_games_to_play = 20
 
     print(df)
 
-    # df, state = StateInitializer(ReaderFootballDataCoUK(), Model.GeneralSkill(default_dist)).initialize_modelstate("./datasets/SWE.csv")
     team_count = {k:0 for k in state.teams.keys()}
-    print(team_count)
+    print(f"{team_count=}")
     state.home_advantage = initial_home_advantage_dist
     # print(df, state)
     # print(state.teams.keys())
     # print(state.teams.get("AIK"))
     # print(state.teams.get("Orebro"))
 
-    model = Model.TrueskillAdvancedAttackDefense(Model.DEFAULT_VAR_T, Model.DEFAULT_VAR_T)
-    adapter = Model.TrueskillAdvancedAttackDefenseAdapter(model)
     # model = Model.TrueskillAdvanced(Model.DEFAULT_VAR_T, 1)
     # adapter = Model.TrueskillAdvancedAdapter(model)
     engine = Model.GameEngine(adapter, state)
@@ -182,7 +146,7 @@ def test():
         pred = engine.predict(game)
         market_odds = Prediction.MarketOdds.from_floats(row["home_decimal"], row["away_decimal"], row["tie_decimal"])
         #NOTE: De måste ha kört ett antal gånger först
-        if team_count[home] > 10 and team_count[away] > 10:
+        if team_count[home] > min_observed_games_to_play and team_count[away] > min_observed_games_to_play:
             bet_lst = strategy.decide(pred, market_odds)
         else:
             print(f"skipping bet on {game}")
